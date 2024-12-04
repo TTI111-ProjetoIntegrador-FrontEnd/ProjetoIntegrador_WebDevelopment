5a7f2b08f04e3d2f12120586dcec46813bcc5d51
PedroVasPassos <pdvasconcelos01@gmail.com>
2024-12-04 19:02:18 -0300

    mexi na tela tudosobre e ajeitei a de membros

--- Front-End/JS/acessibilidade.js
+++ Front-End/JS/acessibilidade.js
@@ -1,11 +1,15 @@
 daltonismoSelect.addEventListener('change', function() {
-
+    console.log("Evento disparado");
+    console.log("Valor selecionado:", daltonismoSelect.value);
+    
+    // Remover as classes anteriores de daltonismo
     document.body.classList.remove('protanopia', 'deuteranopia', 'tritanopia');
     
-
+    // Captura o valor selecionado
     const selectedValue = daltonismoSelect.value;
+
+    // Se um valor for selecionado, adiciona a classe correspondente
     if (selectedValue) {
-        document.body.classList.add(selectedValue);
+        document.body.classList.add(selectedValue);  // Adiciona a classe ao body
     }
 });
-document.body.classList.add(selectedClass);
